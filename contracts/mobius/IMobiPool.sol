43063d855eaa8d5d85fd88ef35a682ff94233d76
viraj124 <virajm72@gmail.com>
2021-11-18 13:33:10 +0530

    refactor: add mobius interfaces

---
+++ contracts/mobius/IMobiPool.sol
@@ -0,0 +1,37 @@
+pragma solidity >=0.6.11;
+
+interface ICurvePool {
+    function addLiquidity(
+        uint256[] calldata amounts,
+        uint256 minToMint,
+        uint256 deadline
+    ) external returns (uint256);
+
+    function remove_liquidity_one_coin(
+        uint256 _token_amount,
+        int128 i,
+        uint256 _min_amount,
+        bool _use_underlying
+    ) external returns (uint256);
+
+    function removeLiquidityOneToken(
+        uint256 tokenAmount,
+        uint8 tokenIndex,
+        uint256 minAmount,
+        uint256 deadline
+    ) external returns (uint256);
+
+    function getLpToken() external view returns (address);
+
+    function calculateTokenAmount(
+        address account,
+        uint256[] calldata amounts,
+        bool deposit
+    ) external view returns (uint256);
+
+    function calculateRemoveLiquidityOneToken(
+        address account,
+        uint256 tokenAmount,
+        uint8 tokenIndex
+    ) external view returns (uint256 availableTokenAmount);
+}
