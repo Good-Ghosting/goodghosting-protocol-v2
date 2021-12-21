ce2f9ea28ddc0f5185004a85cd8a94e55b417c85
viraj124 <virajm72@gmail.com>
2021-12-21 17:53:21 +0530

    refactor: add interfaces to support wmatic and matic pools with aave strategy

---
+++ contracts/aave/IWETHGateway.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.7;
+
+interface IWETHGateway {
+    function depositETH(
+        address lendingPool,
+        address onBehalfOf,
+        uint16 referralCode
+    ) external payable;
+
+    function withdrawETH(
+        address lendingPool,
+        uint256 amount,
+        address to
+    ) external;
+}
---
+++ contracts/polygon/WMatic.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8.7;
+
+interface WMatic {
+    function deposit() external payable;
+
+    function withdraw(uint256 wad) external;
+
+    function totalSupply() external view returns (uint256);
+
+    function approve(address guy, uint256 wad) external returns (bool);
+
+    function transfer(address dst, uint256 wad) external returns (bool);
+
+    function transferFrom(
+        address src,
+        address dst,
+        uint256 wad
+    ) external returns (bool);
+}
