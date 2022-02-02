e5bde772f2313dd3336f2fddfa2c2aa65d53f085
viraj124 <virajm72@gmail.com>
2022-02-02 15:22:49 +0530

    chore: added mainnet deployment netowrk for curve strategy based pools via truffle

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,5 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 const Web3 = require("web3");
-
 require("dotenv").config();
 require("ts-node/register");
 
@@ -88,6 +88,23 @@ module.exports = {
       port: 8545,
       network_id: "*",
       gas: 5000000,
+    },
+    "polygon-curve": {
+      provider: () =>
+        new HDWalletProvider(
+          process.env.POLYGON_MAINNET_MNEMONIC,
+          process.env.POLYGON_MAINNET_PROVIDER_URL,
+          0, //address_index
+          10, // num_addresses
+          true, // shareNonce
+        ),
+      network_id: 137, // Polygon mainnet id
+      networkCheckTimeout: 60000,
+      //gas: 7017622, //
+      gasPrice: 30000000000, // 30 Gwei
+      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 50, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: false, // Skip dry run before migrations? (default: false for public nets )
     },
   },
 
