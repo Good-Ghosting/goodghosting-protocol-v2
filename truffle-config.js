062a8e375dd2bbfd55f5ba25187fbb85a7723541
viraj124 <virajm72@gmail.com>
2022-06-03 13:33:47 +0530

    feat: update deployment script to support test networks

--- truffle-config.js
+++ truffle-config.js
@@ -99,7 +99,7 @@ module.exports = {
         ),
       network_id: 80001, // Polygon testnet id
       networkCheckTimeout: 60000,
-      gasPrice: 200000000000, // 200 Gwei
+      gasPrice: 60000000000, // 200 Gwei
       confirmations: 2, // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 50, // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: false, // Skip dry run before migrations? (default: false for public nets )
