71d36dfc69e4ac66e8cfef8f91e0a3d6f8ab94ba
viraj124 <virajm72@gmail.com>
2022-09-22 12:32:47 +0530

    chore: add natspec dcos in amm interfaces

--- contracts/mobius/IMobiPool.sol
+++ contracts/mobius/IMobiPool.sol
@@ -4,12 +4,23 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface IMobiPool {
 
+    /**
+    @param amounts deposit amounts.
+    @param minToMint min amount based on slippage.
+    @param deadline timestamp value after this tx would revert.
+    */
     function addLiquidity(
         uint256[] calldata amounts,
         uint256 minToMint,
         uint256 deadline
     ) external returns (uint256);
 
+    /**
+    @param tokenAmount token amount to be removed.
+    @param tokenIndex token index of the pool.
+    @param minAmount min amount based on slippage.
+    @param deadline timestamp value after this tx would revert.
+    */
     function removeLiquidityOneToken(
         uint256 tokenAmount,
         uint8 tokenIndex,
@@ -19,12 +30,22 @@ interface IMobiPool {
 
     function getLpToken() external view returns (address);
 
+    /**
+    @param account liquidity holder in the pool.
+    @param amounts deposit amounts.
+    @param deposit flag indicating a deposit.
+    */
     function calculateTokenAmount(
         address account,
         uint256[] calldata amounts,
         bool deposit
     ) external view returns (uint256);
 
+    /**
+    @param account liquidity holder in the pool.
+    @param tokenAmount oken amount to be removed.
+    @param tokenIndex token index of the pool.
+    */
     function calculateRemoveLiquidityOneToken(
         address account,
         uint256 tokenAmount,
