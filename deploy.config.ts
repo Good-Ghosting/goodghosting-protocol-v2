45a100781238992a7fce92e3e661f91294c54d46
viraj124 <virajm72@gmail.com>
2022-11-09 17:20:00 +0530

    chore: update sample .env

--- deploy.config.ts
+++ deploy.config.ts
@@ -27,7 +27,7 @@ exports.deployConfigs = {
   maxPlayersCount: MAX_PLAYER_COUNT, // max quantity of players allowed.
   // merkleroot: "0xd566243e283f1357e5e97dd0c9ab0d78177583074b440cb07815e05f615178bf", // merkle root for 1st 4 player addresses in the fork tests
   isWhitelisted: false,
-  incentiveToken: "0x430EF9263E76DAE63c84292C3409D61c598E9682", // for fork tests, polygon incentive token 0x430EF9263E76DAE63c84292C3409D61c598E9682, 0x1c954e8fe737f99f68fa1ccda3e51ebdb291948c(matic) & celo incentive token 0x46c9757c5497c5b1f2eb73ae79b6b67d119b0b58
+  incentiveToken: "0x430EF9263E76DAE63c84292C3409D61c598E9682", // for fork tests, polygon incentive token 0x430EF9263E76DAE63c84292C3409D61c598E9682, 0x1c954e8fe737f99f68fa1ccda3e51ebdb291948c(matic) & celo incentive token 0x46c9757c5497c5b1f2eb73ae79b6b67d119b0b58, st celo(0x84c4e1f2965235f60073e3029788888252c4557e)
   initialize: true,
   rewardTokens: [
     // providers.celo.tokens.symm.address,
