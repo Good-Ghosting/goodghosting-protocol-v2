e1f04e2cdd554362371babc97838c05100809a84
fodisi <36540281+fodisi@users.noreply.github.com>
2023-02-17 11:18:50 -0500

    Apply suggestions from code review

--- deploy.config.ts
+++ deploy.config.ts
@@ -12,8 +12,8 @@ const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
 const MAX_PLAYER_COUNT = "18446744073709551615";
 
 exports.deployConfigs = {
-  strategy: "polygon-curve-stmatic-matic",
-  inboundCurrencySymbol: "wmatic", // name of the inbound currency symbol. Must be defined in the object {providers.network} above.
+  strategy: "aaveV3",
+  inboundCurrencySymbol: "dai", // name of the inbound currency symbol. Must be defined in the object {providers.network} above.
   owner: ZERO_ADDRESS, //dummy address,
   depositCount: 2, // integer number of segments
   segmentLength: WEEK, // in seconds
@@ -23,11 +23,11 @@ exports.deployConfigs = {
   maxFlexibleSegmentPaymentAmount: 0,
   segmentPayment: 1, // amount of tokens - i.e. 10 equals to 10 TOKENS (DAI, ETH, etc.);
   earlyWithdrawFee: 1, // i.e. 10 equals to 10%
-  adminFee: 1, // i.e. 5 equals to 5%
+  adminFee: 5, // i.e. 5 equals to 5%
   maxPlayersCount: MAX_PLAYER_COUNT, // max quantity of players allowed.
   // merkleroot: "0xd566243e283f1357e5e97dd0c9ab0d78177583074b440cb07815e05f615178bf", // merkle root for 1st 4 player addresses in the fork tests
   isWhitelisted: false,
-  incentiveToken: "0x1c954e8fe737f99f68fa1ccda3e51ebdb291948c", // for fork tests, polygon incentive token 0x430EF9263E76DAE63c84292C3409D61c598E9682, 0x1c954e8fe737f99f68fa1ccda3e51ebdb291948c(matic) & celo incentive token 0x46c9757c5497c5b1f2eb73ae79b6b67d119b0b58, st celo(0x84c4e1f2965235f60073e3029788888252c4557e)
+  incentiveToken: ZERO_ADDRESS, // for fork tests, polygon incentive token 0x430EF9263E76DAE63c84292C3409D61c598E9682, 0x1c954e8fe737f99f68fa1ccda3e51ebdb291948c(matic) & celo incentive token 0x46c9757c5497c5b1f2eb73ae79b6b67d119b0b58, st celo(0x84c4e1f2965235f60073e3029788888252c4557e)
   initialize: true,
   rewardTokens: [
     // providers.celo.tokens.symm.address,
